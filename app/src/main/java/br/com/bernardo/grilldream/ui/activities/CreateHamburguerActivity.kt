fcdea414bfa24a0d9e859e675d7c57de5eee3536
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-12-18 22:56:29 -0300

    Added button shapes and rule to click on the buttons

--- app/src/main/java/br/com/bernardo/grilldream/ui/activities/CreateHamburguerActivity.kt
+++ app/src/main/java/br/com/bernardo/grilldream/ui/activities/CreateHamburguerActivity.kt
@@ -2,6 +2,7 @@ package br.com.bernardo.grilldream.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import br.com.bernardo.grilldream.Adapter.CarousselAdapter
 import br.com.bernardo.grilldream.R
 import br.com.bernardo.grilldream.databinding.ActivityCreateHamburguerBinding
@@ -23,5 +24,90 @@ class CreateHamburguerActivity : AppCompatActivity() {
 
         val viewPager = binding.viewPager
         viewPager.adapter = CarousselAdapter(listImages)
+
+        binding.btnAcem.setOnClickListener {
+            it.setBackgroundResource(R.drawable.rounded_button_enable)
+            binding.btnAlcatra.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnPatinho.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnWagyu.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnPaleta.setBackgroundResource(R.drawable.rounded_button_disable)
+        }
+
+        binding.btnAlcatra.setOnClickListener {
+            it.setBackgroundResource(R.drawable.rounded_button_enable)
+            binding.btnAcem.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnPatinho.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnWagyu.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnPaleta.setBackgroundResource(R.drawable.rounded_button_disable)
+        }
+
+        binding.btnPatinho.setOnClickListener {
+            it.setBackgroundResource(R.drawable.rounded_button_enable)
+            binding.btnAlcatra.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnAcem.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnWagyu.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnPaleta.setBackgroundResource(R.drawable.rounded_button_disable)
+        }
+
+        binding.btnWagyu.setOnClickListener {
+            it.setBackgroundResource(R.drawable.rounded_button_enable)
+            binding.btnAlcatra.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnPatinho.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnAcem.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnPaleta.setBackgroundResource(R.drawable.rounded_button_disable)
+        }
+
+        binding.btnPaleta.setOnClickListener {
+            it.setBackgroundResource(R.drawable.rounded_button_enable)
+            binding.btnAlcatra.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnPatinho.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnWagyu.setBackgroundResource(R.drawable.rounded_button_disable)
+            binding.btnAcem.setBackgroundResource(R.drawable.rounded_button_disable)
+        }
+
+        binding.btnAlface.setOnClickListener {
+            if (it.background.constantState == ContextCompat.getDrawable(this, R.drawable.rounded_button_enable)?.constantState) {
+                it.setBackgroundResource(R.drawable.rounded_button_disable)
+            }
+            else {
+                it.setBackgroundResource(R.drawable.rounded_button_enable)
+            }
+        }
+
+        binding.btnTomate.setOnClickListener {
+            if (it.background.constantState == ContextCompat.getDrawable(this, R.drawable.rounded_button_enable)?.constantState) {
+                it.setBackgroundResource(R.drawable.rounded_button_disable)
+            }
+            else {
+                it.setBackgroundResource(R.drawable.rounded_button_enable)
+            }
+        }
+
+        binding.btnQueijo.setOnClickListener {
+            if (it.background.constantState == ContextCompat.getDrawable(this, R.drawable.rounded_button_enable)?.constantState) {
+                it.setBackgroundResource(R.drawable.rounded_button_disable)
+            }
+            else {
+                it.setBackgroundResource(R.drawable.rounded_button_enable)
+            }
+        }
+
+        binding.btnCebola.setOnClickListener {
+            if (it.background.constantState == ContextCompat.getDrawable(this, R.drawable.rounded_button_enable)?.constantState) {
+                it.setBackgroundResource(R.drawable.rounded_button_disable)
+            }
+            else {
+                it.setBackgroundResource(R.drawable.rounded_button_enable)
+            }
+        }
+
+        binding.btnPicles.setOnClickListener {
+            if (it.background.constantState == ContextCompat.getDrawable(this, R.drawable.rounded_button_enable)?.constantState) {
+                it.setBackgroundResource(R.drawable.rounded_button_disable)
+            }
+            else {
+                it.setBackgroundResource(R.drawable.rounded_button_enable)
+            }
+        }
     }
 }
